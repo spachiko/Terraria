e60f80c9b2be942b7a0ee07ac395eca7d86af5e8
shelbyyz <breadcatisbaking@gmail.com>
2025-05-18 17:00:21 -0400

    Fix Build Issues
    Project was skipping building, therefore unable to run

--- Terraria/Properties/AssemblyInfo.cs
+++ Terraria/Properties/AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Terraria")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f6e9c392694afdbd64fca21dd979b4fdf2063e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Terraria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Terraria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
